38707e2b929c6b91a05bd09229b1f1e366dbc859
Salakar <mike.diarmid@gmail.com>
2023-01-19 21:00:24 +0000

    fix asyncness issue

--- example/lib/fetch_api.dart
+++ example/lib/fetch_api.dart
@@ -6,24 +6,11 @@ import 'package:js/js_util.dart';
 import 'fetch_interop.dart' as interop;
 import 'fetch_interop.dart';
 
-void addFetchEventListener(void Function(FetchEvent event) listener) {
+void addFetchEventListener(Null Function(FetchEvent event) listener) {
   interop.addEventListener<interop.FetchEvent>('fetch',
       js.allowInterop((interop.FetchEvent delegate) {
     listener(FetchEvent._(delegate));
   }));
-}
-
-Stream<FetchEvent> get onFetch {
-  late StreamController<FetchEvent> controller;
-  controller = StreamController<FetchEvent>(
-    sync: true,
-    onListen: () {
-      addFetchEventListener((FetchEvent event) {
-        controller.sink.add(event);
-      });
-    },
-  );
-  return controller.stream;
 }
 
 class FetchEvent {
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,30 +1,11 @@
 import 'fetch_api.dart';
 
 void main() {
-  onFetch.where((event) => event.request.url.endsWith('/mike')).listen((event) {
-    event.respondWith(Response('hello-mike'));
-  });
-
-  onFetch
-      .where((event) => event.request.url.endsWith('/elliot'))
-      .listen((event) {
-    event.respondWith(Response('hello-elliot'));
-  });
-
-  onFetch.listen((event) {
-    print(event.request.url);
-    event.respondWith(Response('I don\'t know you'));
-  });
-
   // Alternative none stream syntax:
-  //   addFetchEventListener((FetchEvent event) {
-  //     print(event.request.url);
-  //     if (event.request.url.endsWith('/elliot')) {
-  //       event.respondWith(Response('hello-elliot'));
-  //     } else if (event.request.url.endsWith('/mike')) {
-  //       event.respondWith(Response('hello-mike'));
-  //     } else {
-  //       event.respondWith(Response('I don\'t know you'));
-  //     }
-  //   });
+  addFetchEventListener((FetchEvent event) {
+    event.respondWith(Future(() async {
+      await Future.delayed(Duration(seconds: 1));
+      return Response('hello-mike');
+    }));
+  });
 }
