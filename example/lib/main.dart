fc9035e3ba98d40643e842c92888380995fec31d
Salakar <mike.diarmid@gmail.com>
2023-01-20 18:09:37 +0000

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:fetch_apis/fetch_apis.dart';
 
+const shouldUseCache = false;
+
 void main() {
   addFetchEventListener((FetchEvent event) {
     event.respondWith(Future(() async {
@@ -12,17 +14,26 @@ void main() {
 
       try {
         final cachedResponse = await caches.defaultCache.match(url);
-        if (cachedResponse != null) {
+        if (shouldUseCache && cachedResponse != null) {
           print('Using cached response');
           return cachedResponse;
         }
 
-        final fetchResponse = await fetch(Resource(url.toString()));
+        final fetchResponse = await fetch(Resource.uri(url));
         final buffer = await fetchResponse.arrayBuffer();
-        final response = Response(buffer);
+        final headers = Headers({
+          'content-type': 'application/json',
+          'x-foo': 'bar',
+        });
+        final response = Response(buffer, headers: headers);
+
+        // Make sure we can decode the response ourselves.
         print(Utf8Decoder().convert(buffer.asUint8List()));
-        await caches.defaultCache.put(url, response.clone());
-        return response.clone();
+
+        if (shouldUseCache) {
+          await caches.defaultCache.put(url, response.clone());
+        }
+        return response;
       } catch (e, s) {
         print(s);
         print(e);
