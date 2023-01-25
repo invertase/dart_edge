356df6de4fb754d7223936ca190f6b07b99aa5f0
Elliot Hesp <elliot.hesp@gmail.com>
2023-01-25 13:09:53 +0000

    -

--- packages/cloudflare_workers/bin/main.dart
+++ packages/cloudflare_workers/bin/main.dart
@@ -4,7 +4,8 @@ void main(List<String> arguments) async {
   // TODO handle args (e.g. minification level etc)
 
   // 1. Spawn process to compile dart to js
-  // 2. Create a new file with the js code (see below)
+  // 2. Parse the wranger.toml file, and get out the durable object class nammes
+  // 3. Create a new file with the js code (see below)
 }
 
 // import "./main.dart.js";
@@ -16,3 +17,23 @@ void main(List<String> arguments) async {
 //     }
 //   },
 // };
+
+// export class ElliotDurableObject {
+//   constructor(state, env) {
+//     const instance = self.__durableObjects["ElliotDurableObject"];
+//     instance.state = state;
+//     instance.env = env;
+
+//     if (!instance) {
+//       throw new Error(
+//         `No Dart Durable object instance named 'ElliotDurableObject' found.`
+//       );
+//     }
+
+//     this.delegate = instance;
+//   }
+
+//   async fetch(request, env) {
+//     return this.delegate.fetch(request);
+//   }
+// }
