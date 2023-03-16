25fb313c4087bc7eeaa92b1b5fe0fd267439dd14
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2023-03-16 12:05:43 +0100

    add more package:http tests

--- packages/edge_runtime/test/http_client_test.dart
+++ packages/edge_runtime/test/http_client_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:edge_runtime/edge_runtime.dart';
 import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
@@ -36,6 +38,48 @@ void main() {
 
         final responseChunks = await res.stream.toList();
         expect(responseChunks.length, greaterThan(1));
+      }));
+
+      group('with headers', () {
+        test('content-type', run(() async {
+          final Uri uri = Uri.parse('http://localhost:3001/200/json');
+          final res = await http.get(uri);
+
+          expect(res.statusCode, 200);
+          expect(res.headers['content-type'], 'application/json');
+        }));
+      });
+    });
+
+    test('sends all headers', run(() async {
+      final uri = Uri.parse('http://localhost:3001/200/echoHeaders');
+
+      const headers = {
+        'accept': 'application/json',
+        'connection': 'keep-alive',
+        'x-custom-header': 'custom-value',
+      };
+
+      final res = await http.get(uri, headers: headers);
+
+      expect(res.statusCode, 200);
+
+      final body = jsonDecode(res.body) as Map<String, dynamic>;
+      for (var key in headers.keys) {
+        expect(body[key], headers[key]);
+      }
+    }));
+
+    group('Response', () {
+      test('has a correct contentLength property', run(() async {
+        int contentLength = 100;
+
+        final uri = Uri.parse(
+          'http://localhost:3001/200/bytes?length=$contentLength',
+        );
+
+        final res = await http.get(uri);
+        expect(res.contentLength, contentLength);
       }));
     });
   });
