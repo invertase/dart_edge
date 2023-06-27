15d081ac34e73502c0c2c50536621c8a051aa4ec
Gaëtan <blaxoujunior@gmail.com>
2023-06-27 14:51:23 +0200

    Update packages/edge_runtime/lib/src/utils.dart
    Co-authored-by: Remi Rousselet <darky12s@gmail.com>

--- packages/edge_runtime/lib/src/utils.dart
+++ packages/edge_runtime/lib/src/utils.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data' show ByteBuffer, Uint8List;
 
 Object? convertBody(Object? body) {
   return switch (body) {
-    String || Uint8List || ByteBuffer || null => body,
+    String() || Uint8List() || ByteBuffer() || null => body,
     _ => throw ArgumentError.value(
         body,
         'body',
